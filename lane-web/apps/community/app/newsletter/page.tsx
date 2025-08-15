83641cbbf7e7798a2dbacc7ba38600dbec9913cd
Noma9363 <noma9363@gmail.com>
2025-08-15 17:03:48 +0900

    feat: add NewsLetter page component
    - will update asap

---
+++ lane-web/apps/community/app/newsletter/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Headline} from "@/app/components/atoms/headline/Headline";
+
+export default function NewsLetterPage() {
+    return (
+        <div>
+           <Headline headingLevel="h1">HOT</Headline>
+        </div>
+    )
+}
